1ab7abcea31a4aac57cefdbfd77c1174bc0f58ef
atharvanaik <atharvanaik026@gmail.com>
2026-08-31 04:50:31 +0530

    Fix Azure trust proxy for correct HTTPS OAuth redirect; deployment confirmed working end-to-end

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ import autoOrderRouter from "./routes/autoOrder.js";
 import { startInsightScheduler } from "./cron/insightCron.js";
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
